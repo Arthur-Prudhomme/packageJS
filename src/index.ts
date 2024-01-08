5e4c518734cd9debb7eb119eb2607abfcca64641
Félix Laviéville <felix@oplit.fr>
2024-01-08 10:59:01 +0100

    feat: debut d'implementation du robot + style files

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,40 @@
 class Mouche {
-    estPose = false;
+	static toutesMouches: Mouche[] = [];
+	estPose: boolean;
+	constructor() {
+		Mouche.toutesMouches.push(this);
+		this.estPose = false;
+	}
 
-    /**
-     * 
-     * @param {Cadav} cadav
-     * @returns {boolean} est posé ou pas
-     */
-    pose(cadav: Cadav): boolean {
-        if (!(cadav instanceof Cadav)) return false;
-        if (cadav.estMouchePosee(this)) return true;
-        cadav.mouches.push(this);
-        return true;
-    }
+	/**
+	 * Permet de déposer cette mouche sur le cadav passé en paramètre. Sauvegarde la mouche dans l'objet cadav
+	 * @param {Cadav} cadav
+	 * @returns {boolean} est posé ou pas
+	 */
+	pose(cadav: Cadav): boolean {
+		if (!(cadav instanceof Cadav)) return false;
+		if (cadav.estMouchePosee(this)) return true;
+		cadav.mouches.push(this);
+		this.estPose = true;
+		return true;
+	}
 }
 
 class Cadav {
-    mouches: Mouche[] = [];
-    estMouchePosee(mouche: Mouche) {
-        return this.mouches.some((m: Mouche) => m === mouche)
-    }
+	mouches: Mouche[] = [];
+	estMouchePosee(mouche: Mouche) {
+		return this.mouches.some((m: Mouche) => m === mouche);
+	}
 }
 
+type MaybeMouche = Mouche | undefined;
+
 class Robot {
-    detek(mouche) {}
-    reverse_detek(cadav) {}
+	detek(mouche: Mouche): boolean {
+		return mouche.estPose;
+	}
+	findMouche(): MaybeMouche {
+		return Mouche.toutesMouches.find((mouche: Mouche) => mouche.estPose);
+	}
+	reverseDetek(cadav) {}
 }
