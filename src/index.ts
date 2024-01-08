e6a3cbfd2418e3bfcfb68380e36a9e6fbc418701
Arthur Prudhomme <arthurprudhommemail06@gmail.com>
2024-01-08 16:29:30 +0100

    feat : added arrayToLowerCase() and arrayToUpperCase()

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,35 @@
-export class Mouche {
-	static toutesMouches: Mouche[] = [];
-	estPose: boolean;
-	constructor() {
-		Mouche.toutesMouches.push(this);
-		this.estPose = false;
-	}
-
-	/**
-	 * Permet de déposer cette mouche sur le cadav passé en paramètre. Sauvegarde la mouche dans l'objet cadav
-	 * @param {Cadav} cadav
-	 * @returns {boolean} est posé ou pas
-	 */
-	pose(cadav: Cadav): boolean {
-		if (!(cadav instanceof Cadav)) return false;
-		if (cadav.estMouchePosee(this)) return true;
-		cadav.mouches.push(this);
-		this.estPose = true;
-		return true;
-	}
-}
-
-export class Cadav {
-	mouches: Mouche[] = [];
-	estMouchePosee(mouche: Mouche) {
-		return this.mouches.some((m: Mouche) => m === mouche);
-	}
-}
-
-type MaybeMouche = Mouche | undefined;
-
-export class Robot {
-	/**
-	 * Permet de vérifier si la  mouche est posé
-	 * @param {Mouche} mouche
-	 * @returns {boolean} vérifie si est posé ou pas
-	 */
-	detek(mouche: Mouche): boolean {
-		return mouche.estPose;
-	}
-
-	/**
-	 * Permet de trouver LA mouche dans toutes les mouches
-	 * @returns {MaybeMouche} mouche quantique
-	 */
-	findMouche(): MaybeMouche {
-		return Mouche.toutesMouches.find((mouche: Mouche) => mouche.estPose);
-	}
-
-	/**
-	 * Permet de savoir si un cadav a été touché par une mouche
-	 * @param {Cadav} cadav
-	 * @returns {boolean} vérifie si une mouche s'est posé ou pas
-	 */
-	//reverseDetek(cadav) {}
-
-	/**
-	 * Permet de savoir si un cadav a été touché par une mouche
+/**
+	 * Calcul de la moyenne d'un tableau de nombre
 	 * @param {number[]} tableau
-	 * @returns {number} vérifie si une mouche s'est posé ou pas
+	 * @returns {number} moyenne
 	 */
-	calculMoyenne(tableau: number[]) {
-		let sum = tableau.reduce((a, b) => a + b, 0);
-		return Math.round(sum / tableau.length);
-	}
+export function calculMoyenne(tableau: number[]) {
+	let sum = tableau.reduce((a, b) => a + b, 0);
+	return Math.round(sum / tableau.length);
+}
+
+/**
+	 * Met toutes les valeurs d'un tableau de string en minuscules
+	 * @param {string[]} tableau
+	 * @returns {string[]} tableau en minuscules
+	 */
+export function arrayToLowerCase(tableau: string[]) {
+	const convertTableau: string[] = [];
+	tableau.forEach(element => {
+		convertTableau.push(element.toLowerCase());
+	});
+	return convertTableau;
+}
+
+/**
+	 * Met toutes les valeurs d'un tableau de string en majuscules
+	 * @param {string[]} tableau
+	 * @returns {string[]} tableau en majuscules
+	 */
+export function arrayToUpperCase(tableau: string[]): string[] {
+	const convertTableau: string[] = [];
+	tableau.forEach(element => {
+		convertTableau.push(element.toUpperCase());
+	});
+	return convertTableau;
 }
